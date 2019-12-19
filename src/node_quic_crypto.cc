48e95217ffd8ae880dcf133a9e5bf5da9aa97d4b
James M Snell <jasnell@gmail.com>
2019-12-19 12:13:01 -0800

    quic: doc comments and cleanups

--- src/node_quic_crypto.cc
+++ src/node_quic_crypto.cc
@@ -113,8 +113,7 @@ void GenerateRandData(uint8_t* buf, size_t len) {
 
 bool GenerateResetToken(
     uint8_t* token,
-    uint8_t* secret,
-    size_t secretlen,
+   const ResetTokenSecret& secret,
     const ngtcp2_cid* cid) {
   ngtcp2_crypto_ctx ctx;
   ngtcp2_crypto_ctx_initial(&ctx);
@@ -122,8 +121,8 @@ bool GenerateResetToken(
       token,
       NGTCP2_STATELESS_RESET_TOKENLEN,
       &ctx.md,
-      secret,
-      secretlen,
+      secret.data(),
+      secret.size(),
       cid->data,
       cid->datalen));
 }
